a13b87459a1242407c4fea39b8105e66bdccf7a1
Mink Triet <user@usernoMacBook-Pro.local>
2024-09-20 15:33:05 +0700

    better conv

---
+++ README.md
@@ -0,0 +1,31 @@
+# OSA detection
+
+## Using the CLI
+
+```zsh
+python3 src/model.py -h
+```
+
+## Data
+
+### Preprocess data
+
+```zsh
+python3 src/preprocess.py
+```
+
+### Data used:
+
+Dataset: [Apnea-ECG-Database](https://physionet.org/content/apnea-ecg/1.0.0/)
+
+#### Patients used
+
+1. a01r
+1. a02r
+1. a03r
+1. a04r
+1. b01r
+1. c01r
+1. c02r
+1. c03r
+    ##### Patients used for the training data: b2 3 4 5 _(recommended)_
--- src/model.py
+++ src/model.py
@@ -10,6 +10,7 @@ import argparse
 from keras.utils import to_categorical
 from keras import optimizers
 from sklearn.utils import shuffle
+from collections import Counter
 
 def get_patients(plist):
     def get_patient(patientid):
@@ -27,11 +28,12 @@ def get_patients(plist):
         siglen += len(ann)
     
     X = np.array(np.split(X, siglen, axis=1))
+    X = np.array([rec.T for rec in X])
     return X, y
 
 records = ["a01r", "a02r", "a03r", "a04r", "b01r", "c01r", "c02r", "c03r"]
 batch_size = 64
-epochs = 5
+epochs = 4
 save_path = path.join("res", "model.keras")
 
 parser = argparse.ArgumentParser(description='Train and evaluate on multiple patients')
@@ -49,12 +51,15 @@ eval_list = [int(x)-1 for x in eval_list]
 
 if patient_list != []:
     model = Sequential([
-        layers.Input(shape=(2, 6000)),
-        layers.Conv1D(filters=128, kernel_size=1, activation='relu'),
-        layers.MaxPooling1D(pool_size=2),
-        layers.Conv1D(filters=256, kernel_size=1, activation='relu'),
+        layers.Input(shape=(6000, 2)),
+        layers.Conv1D(filters=16, kernel_size=3, activation='relu'),
+        layers.MaxPooling1D(pool_size=4),
+        layers.Conv1D(filters=32, kernel_size=3, activation='relu'),
+        layers.MaxPooling1D(pool_size=4),
+        layers.Conv1D(filters=64, kernel_size=3, activation='relu'),
+        layers.MaxPooling1D(pool_size=4),
         layers.Flatten(),
-        layers.Dense(512, activation="relu"),
+        layers.Dense(128, activation="relu"),
         layers.Dense(1, activation='sigmoid')
     ])
     model.compile(
@@ -64,6 +69,8 @@ if patient_list != []:
     )
     X, y = get_patients(patient_list)
     X, y = shuffle(X, y, random_state=22022009)
+    count = Counter(y)
+    print("Apnea cases [1]:", count[1], "Normal cases [0]:", count[0])
     model.fit(
         X, y,
         batch_size=batch_size,
@@ -75,4 +82,5 @@ if patient_list != []:
 if eval_list != []:
     model = load_model(save_path)
     X, y = get_patients(eval_list)
+    X, y = shuffle(X, y, random_state=22022009)
     model.evaluate(X, y, batch_size=batch_size)
